d073df6d0f7b26b5ebe9942d91263863b104bbdd
Le-Quoc-Minh <minhlq@epalsoft.net>
2024-03-17 23:16:16 +0700

    Kết nối database

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Import service app
+const serviceApp = require('./service');
+
+// Sử dụng các route từ serviceApp
+app.use(serviceApp);
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+    res.send('Hello World from index.js!');
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
-})
+});
