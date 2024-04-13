6253d574997f0c58767576298b4edfd2b1410f00
Le-Quoc-Minh <minhlq@epalsoft.net>
2024-04-13 11:06:31 +0700

    NB-1 Kiến thức cốt lỗi - static file, scss, boostrap, routing, method, template handlers

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
+
 const express = require('express');
+var handlebars = require('express-handlebars');
 const app = express();
+const path = require('path');
 const port = 3000;
 
-// Import service app
-const serviceApp = require('./service');
 
-// Sử dụng các route từ serviceApp
-app.use(serviceApp);
+// // Import service app
+// const serviceApp = require('./service');
+
+// // Sử dụng các route từ serviceApp
+// app.use(serviceApp);
+
+app.use(express.static('src/public'));
+
+//template engine
+app.engine('hbs', handlebars({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'src\\resources\\views'));
 
 app.get('/', (req, res) => {
-    res.send('Hello World from index.js!');
+    res.render('home');
+});
+
+app.get('/news', (req, res) => {
+    res.render('news');
+});
+
+app.get('/search', (req, res) => {
+    console.log(req.query.q);
+    res.render('search');
 });
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
+
+
--- service.js
+++ service.js
@@ -1,49 +1,49 @@
-const express = require('express');
-const mysql = require('mysql');
+// const express = require('express');
+// const mysql = require('mysql');
 
-const app = express();
-const port = 3000;
+// const app = express();
+// const port = 3000;
 
-// Kết nối đến cơ sở dữ liệu MySQL
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'username',
-    password: 'password',
-    database: 'mydatabase'
-});
+// // Kết nối đến cơ sở dữ liệu MySQL
+// const db = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'test'
+// });
 
-// Kiểm tra kết nối
-db.connect((err) => {
-    if (err) {
-        throw err;
-    }
-    console.log('MySQL Connected...');
-});
+// // Kiểm tra kết nối
+// db.connect((err) => {
+//     if (err) {
+//         throw err;
+//     }
+//     console.log('MySQL Connected...');
+// });
 
-// Lắng nghe các request POST
-app.use(express.json());
+// // Lắng nghe các request POST
+// app.use(express.json());
 
-// Tạo một route GET để lấy danh sách users
-app.get('/users', (req, res) => {
-    let sql = 'SELECT * FROM users';
-    db.query(sql, (err, result) => {
-        if (err) {
-            throw err;
-        }
-        res.send(result);
-    });
-});
+// // Tạo một route GET để lấy danh sách users
+// app.get('/users', (req, res) => {
+//     let sql = 'SELECT * FROM users';
+//     db.query(sql, (err, result) => {
+//         if (err) {
+//             throw err;
+//         }
+//         res.send(result);
+//     });
+// });
 
-// Tạo một route POST để thêm mới user
-app.post('/users', (req, res) => {
-    let newUser = req.body;
-    let sql = 'INSERT INTO users SET ?';
-    db.query(sql, newUser, (err, result) => {
-        if (err) {
-            throw err;
-        }
-        res.send('User added successfully');
-    });
-});
+// // Tạo một route POST để thêm mới user
+// app.post('/users', (req, res) => {
+//     let newUser = req.body;
+//     let sql = 'INSERT INTO users SET ?';
+//     db.query(sql, newUser, (err, result) => {
+//         if (err) {
+//             throw err;
+//         }
+//         res.send('User added successfully');
+//     });
+// });
 
-module.exports = app; // Export app để có thể sử dụng trong index.js
+// module.exports = app; // Export app để có thể sử dụng trong index.js
