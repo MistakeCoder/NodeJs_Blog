b34538e8bfdc1a8f947d1e69039f9bc4976d066e
Le-Quoc-Minh <minhlq@epalsoft.net>
2024-04-24 09:50:24 +0700

    NB-4 Tạo khoá học

--- src/routes/dist/courses.dev.js
+++ src/routes/dist/courses.dev.js
@@ -6,6 +6,9 @@ var router = express.Router();
 
 var CourseController = require('../app/controllers/CourseController');
 
+router.post('/update_courses', CourseController.update_courses);
+router.post('/store_courses', CourseController.store_courses);
+router.get('/create', CourseController.create_courses);
 router.get('/:slug', CourseController.detail_courses);
 router.get('/', CourseController.list_courses);
 module.exports = router;
